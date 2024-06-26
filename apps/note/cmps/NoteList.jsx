c085028c66c7c2b5de3ea49aa9cc73af175ff068
Daniel Vinitsky <danielvinisky@gmail.com>
2024-06-26 12:04:53 +0300

    Add note service demo data

--- apps/note/cmps/NoteList.jsx
+++ apps/note/cmps/NoteList.jsx
@@ -1,3 +1,43 @@
-export function NoteList() {
-    return <div>note list</div>
+import { noteService } from "../services/note.service.js"
+
+const { useState, useEffect, Fragment } = React
+
+export function NoteList({ notes }) {
+    const [pinnedNotes, setPinnedNotes] = useState([])
+    const [unPinnedNotes, setUnPinnedNotes] = useState([])
+
+    useEffect(() => {
+        setPinnedNotes(noteService.getPinnedNotes(notes))
+        setUnPinnedNotes(noteService.getUnPinnedNotes(notes))
+    }, [notes])
+
+    return (
+        <section className="note-list-container">
+            {/* {pinnedNotes.length !== 0 && (
+                <Fragment>
+                    <h1>Pinned Notes</h1>
+                    <section className="pinned-notes">
+                        {pinnedNotes.map(note => (
+                            <div className="note-card" key={note.id}>
+                                <p> hi </p>
+                            </div>
+                        ))}
+                    </section>
+                </Fragment>
+            )} */}
+
+            {/* {unPinnedNotes.length !== 0(
+                <Fragment>
+                    <h1>Notes</h1>
+                    <section className="unPinned-notes">
+                        {unPinnedNotes.map(note => (
+                            <div className="note-card" key={note}>
+                                <p>hi</p>
+                            </div>
+                        ))}
+                    </section>
+                </Fragment>
+            )} */}
+        </section>
+    )
 }
--- apps/note/pages/NoteIndex.jsx
+++ apps/note/pages/NoteIndex.jsx
@@ -1,4 +1,30 @@
 
+import { NoteList } from '../cmps/NoteList.jsx'
+import { noteService } from '../services/note.service.js'
+
+const { useState, useEffect } = React
+
 export function NoteIndex() {
-    return <div>note app</div>
+
+    const [notes, setNotes] = useState(null)
+
+    useEffect(() => {
+        loadNotes()
+    }, [])
+
+    function loadNotes() {
+        noteService.query()
+        .then(notes => setNotes(notes))
+        .catch(err => console.log('err:', err))
+    }
+
+    if (!notes) return <div>Loading...</div>
+    console.log(notes)
+    return (
+        <section className="note-main-container">
+            <NoteList 
+            notes={notes}
+            />
+        </section>
+    )
 }
