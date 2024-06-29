cbb3964aebb769bdf812ece36e08c175c4fbc68e
Daniel Vinitsky <danielvinisky@gmail.com>
2024-06-29 13:29:24 +0300

    Add logo, add nav bar hamburger beautiful

--- cmps/AppHeader.jsx
+++ cmps/AppHeader.jsx
@@ -1,16 +1,28 @@
+
+const { useState } = React
 const { Link, NavLink } = ReactRouterDOM
 
 export function AppHeader() {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    function toggleMenu() {
+        setMenuOpen(!menuOpen)
+    }
 
     return <header className="app-header">
         <Link to="/">
-            <h3>LOGO!</h3>
+            <img className="logo" src="./assets/img/logo.svg" />
         </Link>
-        <nav>
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/about">About</NavLink>
-            <NavLink to="/mail">Mail</NavLink>
-            <NavLink to="/note">Note</NavLink>
+        <div className={`hamburger-menu ${menuOpen ? 'open' : ''}`} onClick={toggleMenu}>
+            <div className="bar"></div>
+            <div className="bar"></div>
+            <div className="bar"></div>
+        </div>
+        <nav className={`nav-links ${menuOpen ? 'open' : ''}`}>
+            <NavLink to="/"><span className="icon"><i className="fa-solid fa-house"></i></span></NavLink>
+            <NavLink to="/about"><span className="icon"><i className="fa-regular fa-address-card"></i></span></NavLink>
+            <NavLink to="/mail"><span className="icon"><i className="fa-regular fa-envelope"></i></span></NavLink>
+            <NavLink to="/note"><span className="icon"><i className="fa-regular fa-note-sticky"></i></span></NavLink>
         </nav>
     </header>
 }
